4a16b0f3112d5f3441b9b63bc8990c8100589558
chip <huongctt3@onemount.com>
2021-08-16 10:12:34 +0700

    Convert Str to LowerCase

--- src/main/java/BaiTH6.java
+++ src/main/java/BaiTH6.java
@@ -48,9 +48,10 @@ public class BaiTH6 {
     }
 
     public static int countVowel(String str){
+        String tmp = str.toLowerCase();
         int count =0;
-        for (int i = 0; i < str.length() ; i++) {
-            if(str.substring(i,i+1).equals("u")||str.substring(i,i+1).equals("e")||str.substring(i,i+1).equals("o")||str.substring(i,i+1).equals("a")||str.substring(i,i+1).equals("i")){
+        for (int i = 0; i < tmp.length() ; i++) {
+            if(tmp.substring(i,i+1).equals("u")||tmp.substring(i,i+1).equals("e")||tmp.substring(i,i+1).equals("o")||tmp.substring(i,i+1).equals("a")||tmp.substring(i,i+1).equals("i")){
                 count = count+1;
             }
 

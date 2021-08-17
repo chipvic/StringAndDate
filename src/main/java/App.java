2490bfd7f2e58a9ed3bd238b2479854e40ea4f55
chip <huongctt3@onemount.com>
2021-08-17 19:17:25 +0700

    BaiTH6

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,47 +1,49 @@
 import java.util.Locale;
 
 public class App {
-    public static void main(String[] args){
-
-//        String s4 = "java";
-//        String s5 = s4.toLowerCase();
-//        char c = s4.charAt(0);
-//        System.out.println(c);
-//
-//        int idx = s4.indexOf("j");
-//        System.out.println(idx);
-//
-//        int idxSubStr = s4.indexOf("va");
-//        System.out.println(idxSubStr);
-//
-//        String s9 = "Java for OneMount Group";
-//        int idxSubS9 = s9.indexOf("Group");
-//        System.out.println(s9.substring(9));
-//        System.out.println(s9.substring(9,17));;
-//        System.out.println(idxSubS9);
+    public static void main(String[] args) {
 
         // Bài 1:
-        System.out.println("Nhập chuỗi và in những ký tự hoa");
+        System.out.println("Nhập chuỗi: ");
         String s = BaiTH6.inputString();
         System.out.println(s);
         BaiTH6.printUpCase(s);
 
         // Bài 2:
-        System.out.println("In chuỗi không bao gồm số");
+        System.out.println("In chuỗi không bao gồm số: ");
         BaiTH6.printNonNum(s);
 
+        // Bài 3:
+        System.out.println("Nhập ký tự cần kiểm tra số lần xuất hiện trong chuỗi");
+        char c = BaiTH6.inputChar();
+        int countC = BaiTH6.findAndCountChar(s,c);
+        System.out.format("Xuất hiện %d lần",countC);
+
+
         // Bài 4
-        int count = BaiTH6.countChar(s);
-        System.out.println(count);
+        System.out.println("In số lượng từ trong chuỗi: ");
+        int countWord = BaiTH6.countWord(s);
+        System.out.println(countWord);
+
+        // Bài 5:
+        System.out.println("Nhập chuỗi cần nối: ");
+        String s2 = BaiTH6.inputString();
+        String s3 =s.concat(s2);
+        System.out.println("Chuỗi sau khi nối là: "+s3);
+
+        // Bài 6:
+        System.out.println(BaiTH6.checkNum(s));
+
 
         // Bài 7:
+        int countChar = BaiTH6.countChar(s);
         int countVowel =BaiTH6.countVowel(s);
-        int countNonVo = count - countVowel;
-        System.out.format("Số lượng từ nguyên âm %s và phụ âm %s",countVowel,countNonVo);
+        int countConsonant = countChar - countVowel;
+        System.out.format("Số lượng từ nguyên âm %s và phụ âm %s",countVowel,countConsonant);
 
-
-
-
+//        // Bài 8:
+//        int a = BaiTH6.freqChar(s);
+//        System.out.println("Ký tự xuất hiện nhiều thứ 2 trong chuỗi: "+a);
 
 
     }
--- src/main/java/BaiTH6.java
+++ src/main/java/BaiTH6.java
@@ -8,6 +8,12 @@ public class BaiTH6 {
         return str;
     }
 
+    public static char inputChar() {
+        Scanner myChar = new Scanner(System.in);
+        char c = myChar.next().charAt(0);
+        return c;
+    }
+
     public static void printUpCase(String str) {
         for (int i = 0; i < str.length(); i++) {
             String tmp = str.substring(i, i + 1);
@@ -24,7 +30,7 @@ public class BaiTH6 {
         for (int i = 0; i < str.length(); i++) {
             if (Character.isDigit(str.charAt(i))) {
             } else {
-                System.out.format("%s ",str.charAt(i));
+                System.out.format("%s ", str.charAt(i));
             }
 
         }
@@ -32,13 +38,49 @@ public class BaiTH6 {
 
     }
 
-    public static int countChar(String str) {
-        int count=0;
+    public static int countWord(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if ((Character.isSpaceChar(str.charAt(i)) && !Character.isSpaceChar(str.charAt(i + 1))) || (i == 0 && !Character.isSpaceChar(str.charAt(i)))) {
+                count += 1;
+            }
+
+        }
+        return count;
+    }
+
+    public static int findAndCountChar(String str, char c) {
+        int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (str.substring(i,i+1).equals(" ")) {
+            if (str.charAt(i) == c) {
+                count += 1;
+            }
+
+        }
+        return count;
+    }
+
+    public static boolean checkNum(String str) {
+        boolean check = true;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                check = false;
+                break;
+            }
+
+        }
+
+        return check;
+
+    }
+
+    public static int countChar(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.substring(i, i + 1).equals(" ")) {
 
             } else {
-                count +=1;
+                count += 1;
 
             }
         }
@@ -47,16 +89,40 @@ public class BaiTH6 {
 
     }
 
-    public static int countVowel(String str){
+
+    public static int countVowel(String str) {
         String tmp = str.toLowerCase();
-        int count =0;
-        for (int i = 0; i < tmp.length() ; i++) {
-            if(tmp.substring(i,i+1).equals("u")||tmp.substring(i,i+1).equals("e")||tmp.substring(i,i+1).equals("o")||tmp.substring(i,i+1).equals("a")||tmp.substring(i,i+1).equals("i")){
-                count = count+1;
+        int count = 0;
+        for (int i = 0; i < tmp.length(); i++) {
+            if (tmp.substring(i, i + 1).equals("u") || tmp.substring(i, i + 1).equals("e") || tmp.substring(i, i + 1).equals("o") || tmp.substring(i, i + 1).equals("a") || tmp.substring(i, i + 1).equals("i")) {
+                count = count + 1;
             }
 
         }
         return count;
+    }
+
+    public static int freqChar(String str) {
+        int no1 = 0;
+        int no2 = 0;
+        int notmp = 0;
+        String tmp = str.trim();
+        tmp = tmp.toLowerCase();
+        for (int i = 0; i < tmp.length(); i++) {
+            notmp = findAndCountChar(tmp, tmp.charAt(i));
+            tmp = tmp.replace(tmp.charAt(i), ' ');
+            if (notmp > no1) {
+                no2 = no1;
+                no1 = notmp;
+            } else {
+                if ((notmp < no1) && (notmp > no2)) {
+                    no2 = notmp;
+                }
+            }
+            tmp = tmp.trim();
+        }
+
+        return no2;
     }
 
 }
